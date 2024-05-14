5a25a5d750d5e5eb6c8668a828fd6f79438919ae
Sauravok <107023000+Sauravok@users.noreply.github.com>
2024-05-14 12:04:06 +0545

    h1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Home from "./component/Home";
 
 function App() {
-  return <h1>test</h1>;
+  return (
+    <>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+
+      <Home />
+    </>
+  );
 }
 
 export default App;
